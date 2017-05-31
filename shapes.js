40172e56ed79e8e3e6fb316c61c2d14b3214e0f9
Ilie Diacov <iliadiacov@mail.ru>
2017-05-30 22:45:32 -0400

    Add Ordinal Scale to shapes

--- shapes.js
+++ shapes.js
@@ -1,4 +1,10 @@
 var dataArray = [5,11,18];
+var dataDays=['Mon','Wed','Fri'];
+var x=d3.scaleBand()
+  .domain(dataDays)
+  .range([0,170])
+  .paddingInner(0.1176);
+var xAxis=d3.axisBottom(x);
 
 var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100%");
 
@@ -10,6 +16,11 @@ svg.selectAll("rect")
     .attr("fill","red")
     .attr("x",function(d,i){ return 60*i; })
     .attr("y",function(d,i){ return 300-(d*15); });
+
+svg.append("g")
+  .attr("class","x axis hidden")
+  .attr("transform","translate(0,300)")
+  .call(xAxis);
 
 var newX=300;
 svg.selectAll("circle.first")
