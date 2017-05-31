0775093f0afe8b55f629b97e5c91414f337f3f2d
Ilie Diacov <iliadiacov@mail.ru>
2017-05-31 00:11:25 -0400

    Add color scales

--- shapes.js
+++ shapes.js
@@ -1,5 +1,7 @@
 var dataArray = [5,11,18];
 var dataDays=['Mon','Wed','Fri'];
+var rainbow=d3.scaleSequential(d3.interpolateRainbow).domain([0,10]);
+var rainbow2=d3.scaleSequential(d3.interpolateRainbow).domain([0,3]);
 var x=d3.scaleBand()
   .domain(dataDays)
   .range([0,170])
@@ -8,12 +10,14 @@ var xAxis=d3.axisBottom(x);
 
 var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100%");
 
+var cat20=d3.schemeCategory20;
+
 svg.selectAll("rect")
   .data(dataArray)
   .enter().append("rect")
     .attr("height",function(d,i){ return d*15; })
     .attr("width","50")
-    .attr("fill","red")
+    .attr("fill",function(d,i){return rainbow(i);})
     .attr("x",function(d,i){ return 60*i; })
     .attr("y",function(d,i){ return 300-(d*15); });
 
@@ -27,6 +31,7 @@ svg.selectAll("circle.first")
   .attr("class","first")
   .data(dataArray)
   .enter().append("circle")
+  .attr("fill",function(d,i){return rainbow2(i);})
   .attr("cx", function(d,i){
     newX+=(d*3)+(i*20);
     return newX;})
@@ -34,10 +39,11 @@ svg.selectAll("circle.first")
   .attr("r",function(d){ return d*3;});
 
 var newX=600;
-svg.selectAll("ellipse.second")
-  .attr("clss","second")
+svg.selectAll("ellipse")
+  .attr("class","second")
   .data(dataArray)
   .enter().append("ellipse")
+  .attr("fill",function(d,i){return cat20[i];})
   .attr("cx", function(d,i){
     newX+=(d*3)+(i*20);
     return newX;})
